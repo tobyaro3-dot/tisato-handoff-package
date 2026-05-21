a3addd783708abaf092ce9d3bb16c2df9bec9593
Alexander Arogundade <Alex@goallinegrowth.com>
2026-05-21 14:23:09 -0400

    Add transparent-style hero video

--- production-starter/src/http/static.js
+++ production-starter/src/http/static.js
@@ -7,9 +7,11 @@ const MIME_TYPES = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".mp4": "video/mp4",
   ".png": "image/png",
   ".svg": "image/svg+xml",
   ".txt": "text/plain; charset=utf-8",
+  ".webm": "video/webm",
   ".xml": "application/xml; charset=utf-8",
 };
 
